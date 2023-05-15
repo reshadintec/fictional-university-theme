cb2211e5db690c5180cdb54db6f7a909173cfd50
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-05-15 17:04:23 +0200

    dynamic sidebar

--- page.php
+++ page.php
@@ -16,25 +16,41 @@ get_header();
     <div class="container container--narrow page-section">
 
       <?php
-         $theParnet =  wp_get_post_parent_id();
-        if ($theParnet) { ?>
+         $theParent =  wp_get_post_parent_id();
+        if ($theParent) { ?>
          <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParnet) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParnet); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+          <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
         </p>
       </div>
        <?php }
       ?>
       
       
-
-      <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <?php
+      $testArray = get_pages(array(
+        'child_of' => get_the_id()
+      ));
+      
+      if($theParent or $testArray) { ?> 
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_the_permalink($theParent)?>"><?php  echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php
+            if($theParent){
+              $findChildrenOf = $theParent;
+            }else{
+              $findChildrenOf = get_the_ID();
+            }
+            wp_list_pages(array(
+              'title_li' => NULL,
+              'child_of' => $findChildrenOf,
+              'sort_column' => 'menu_order' 
+            ));
+          ?>
         </ul>
-      </div> -->
+      </div>
+      <?php } ?>
 
       <div class="generic-content">
         <?php the_content(); ?>

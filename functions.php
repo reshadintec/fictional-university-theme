cc2738c989f772191f46b7c97ff0103fa46d15e7
reshadintec <105705121+reshadintec@users.noreply.github.com>
2023-05-19 18:33:03 +0200

    events done

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php 
 
+
+//adding fonts, styles and js files
 function university_files(){
    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
 
@@ -13,12 +15,43 @@ function university_files(){
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
+//End of adding styles and etc 
+////////////////////////////////
 
+
+//adding menu locations 
 function university_features() {
-   register_nav_menu( 'headerMenuLocation', 'Header Menu Location');
+   register_nav_menu( 'heade rMenuLocation', 'Header Menu Location');
    register_nav_menu( 'footerLocationOne', 'Footer Location One');
    register_nav_menu( 'footerLocationTwo', 'Footer Location Two');
    add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'university_features');
+// End of Menu locations
+/////////////////////////////////////////
+
+
+//custom query for setting up events page 
+
+function university_adjust_queries($query){
+   if(!is_admin() && is_post_type_archive('event') && $query->is_main_query()){
+      $today = date('Ymd');
+      $query -> set('meta_key', 'event_date');
+      $query -> set('orderby', 'meta_value_num');
+      $query -> set('order', 'ASC');
+      $query -> set('meta_query', array(
+         array(
+           'key' => 'event_date',
+           'compare' => '>=', 
+           'value' => $today,
+           'type' => 'numeric'
+         ),
+       ));
+
+   }
+}
+add_action('pre_get_posts','university_adjust_queries');
+
+//End of custom query for events page
+///////////////////////////////////////
